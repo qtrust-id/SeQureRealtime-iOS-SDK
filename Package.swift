035c5ecb2bc2ac678fd15e410d20d4c7ba7702bb
Aaron Venturo <aaronevanjulio@venturo.pro>
2026-02-24 10:19:54 +0700

    feat: add new domain on barcode helper and bump version

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SequreRealtimeSDK",
-            url: "https://github.com/qtrust-id/SeQureRealtime-iOS-SDK/releases/download/1.0.2/SequreRealtimeSDK.xcframework.zip",
-            checksum: "185db910f42242f426e953b839d623815c0950a422db4864aaf21e056ede54cc"
+            url: "https://github.com/qtrust-id/SeQureRealtime-iOS-SDK/releases/download/1.0.3/SequreRealtimeSDK.xcframework.zip",
+            checksum: "78a23ae1e7d40985c5f25e59fa39f5420ae059ce9080e55081902d91a53cefa5"
         ),
     ],
 )
